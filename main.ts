e46b4b0abde4ddd20d853c118477f41892e73840
fyears <hjwdhjwd@gmail.com>
2021-11-25 00:47:22 +0800

    init

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+export default class MyPlugin extends Plugin {
+	async onload() {
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+
+	}
+
+	onunload() {
+	}
+
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		const div = containerEl.createEl('p', {text: 'This is an area for clicking'});
+		div.onClickEvent(x=>{
+			new Notice(`you clicked ${x.detail} times!`)
+		})
+	}
+}
